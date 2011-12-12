3882c5e56cd048acdcf8242e815a7958ea1e36e7
Jaume <jaume@nualart.cat>
2011-12-12 20:50:14 +0100

    run.sh taking arguments

--- run.sh
+++ run.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-echo "Liberating xlsx files..."
-python xlsx2tsv.py data/key.xlsx > data-tmp/key.tsv
-python xlsx2tsv.py data/feedback.xlsx > data-tmp/feedback.tsv
+if [ "$1" = "" ] ; then
+        echo
+        echo "Usage: sh run.sh [export | grouping | relocate]"
+        echo
+fi
 
-echo "Combining 2 TSV files into XML"
-python tsv2xml.py > data-tmp/votations.xml
+if [ "$1" = "export" ] ; then
+    echo "Liberating xlsx files..."
+    python xlsx2tsv.py data/key.xlsx > data-tmp/key.tsv
+    python xlsx2tsv.py data/feedback.xlsx > data-tmp/feedback.tsv
 
-echo "Data analysis + relocation + output"
-python analyzer.py
+    echo "Combining 2 TSV files into XML"
+    python tsv2xml.py > data-tmp/votations.xml
+fi
+
+if [ "$1" = "grouping" ] ; then
+    echo "grouping dummy..."
+fi
+
+if [ "$1" = "relocate" ] ; then
+    echo "Data analysis + relocation + output"
+    python analyzer.py
+fi
+
+echo "bye :-)"
+
+
+
 
